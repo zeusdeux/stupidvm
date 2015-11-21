288915d954af5523536b7f41c48e5adca6a7dcde
Mudit Ameta <zeusdeux@gmail.com>
2015-11-22 00:52:07 +0530

    Added TRUE, FALSE and some error macros

--- vm.h
+++ vm.h
@@ -4,6 +4,23 @@
 #define STUPIDVMMARKER 0xB055BABE
 #define MAX_STACK_SIZE 1000
 
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef EINVALIDJMP
+#define EINVALIDJMP "Invalid address to jump to"
+#endif
+
+#ifndef EINVALIDBC
+#define EINVALIDBC "Invalid bytecode file"
+#endif
+
+
 void run(const int *code, int startingAddr, int trace);
 
 #endif
