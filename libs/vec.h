2466ba00725289fb8824ae7abe38da13f50a89eb
Mudit Ameta <zeusdeux@gmail.com>
2024-01-18 20:00:40 +0530

    Provide a better "looking" API for vec_push

--- libs/vec.h
+++ libs/vec.h
@@ -22,6 +22,8 @@ void vec_push(Vec vec, ...);
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <errno.h>
+#include <string.h>
 #include "util.h"
 
 #define MIN_CAPACITY(element_sz) 4 * (element_sz)
@@ -72,8 +74,15 @@ void vec_resize_(Vec *vec)
   }
 
   size_t new_capacity = vec->capacity * SCALE_FACTOR;
-  vec->data = realloc(vec->data, new_capacity);
-  vec->capacity = new_capacity;
+  void *resized_data_ptr = realloc(vec->data, new_capacity);
+
+  if (resized_data_ptr) {
+    vec->data = resized_data_ptr;
+    vec->capacity = new_capacity;
+  } else {
+    dbg("Vec:Error> Vector resize failed. %s\n", strerror(errno));
+    vec->valid = false;
+  }
 
   dbg_vec_ptr("vec_resize_:output>", vec);
 }
@@ -92,9 +101,11 @@ Vec vec_create_(void *arr, size_t orig_capacity, size_t element_sz)
   size_t length;
   size_t capacity;
 
-  element_sz = element_sz ? element_sz : 4;
-  orig_capacity = orig_capacity ? orig_capacity : MIN_CAPACITY(4);
-  length = arr == NULL ? 0 : orig_capacity / element_sz;
+  element_sz = element_sz ? element_sz : 4; // default to creating an element of width 4 bytes. e.g., int
+  length = arr == NULL ? 0 : orig_capacity / element_sz; // calculate length before setting a default for orig_capacity if it's zero
+  orig_capacity = orig_capacity ? orig_capacity : MIN_CAPACITY(element_sz); // default to 4 elements of width element_sz
+
+  // reserved capacity for vector is always greater than or equal to MIN_CAPACITY(element_sz)
   capacity = orig_capacity < MIN_CAPACITY(element_sz) ? MIN_CAPACITY(element_sz) : orig_capacity;
 
   // allocate contigous bytes, zeroed out
@@ -139,8 +150,32 @@ void vec_free(Vec vec)
   free(vec.data);
 }
 
-#define vec_push(vec, ...) vec_push_((vec), (__typeof__((__VA_ARGS__))[]){__VA_ARGS__}, sizeof((__typeof__((__VA_ARGS__))[]){__VA_ARGS__}))
-void vec_push_(Vec *vec, void *els, size_t reqd_capacity)
+/**
+ * this macro uses non-standard "gnu statement expressions"
+ * clang and gcc both support it
+ * more here: https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html
+ * It uses it to return a correctly typed pointer to vec->data.
+ *
+ * Example:
+ *
+ * Vec v = vec_create(NULL); // create empty vector (defaults to int)
+ * int *arr = (int *)v.data;
+ *
+ * arr = vec_push(v, 200, 300, 400, 500);
+ * printf("a[3] => %d, length => %zu\n", arr[3], v.length); // output: a[3] => 500, length => 4
+ *
+ * arr = vec_push(v, 600, 700, 800); // triggers resize and potential v.data relocation in memory
+ * printf("a[6] => %d, length => %zu\n", arr[6], v.length); // output: a[6] => 800, length => 7
+ *
+ * vec_free(v);
+ */
+#define vec_push(vec, ...)                                              \
+  ({                                                                    \
+    Vec *__zdx_##vec = vec_push_ (&(vec), (__typeof__((__VA_ARGS__))[]){__VA_ARGS__}, sizeof((__typeof__((__VA_ARGS__))[]){__VA_ARGS__})); \
+    (__typeof__((__VA_ARGS__)) *)__zdx_##vec->data;                               \
+  })
+
+Vec *vec_push_(Vec *vec, void *els, size_t reqd_capacity)
 {
   dbg_vec_ptr("vec_push:input>", vec);
   dbg("vec_push:input>\telement ptr %p\trequired capacity %zu", els, reqd_capacity);
@@ -156,20 +191,30 @@ void vec_push_(Vec *vec, void *els, size_t reqd_capacity)
 
     if (reqd_capacity > remaining_capacity) {
       vec_resize_(vec);
+
+      if (!vec->valid) {
+        dbg_vec_ptr("vec_push:error> Invalid vector", vec);
+        return vec;
+      }
     }
 
-    uint8_t *data_ptr = (uint8_t *)vec->data + last_item_offset;
+    uint8_t *start_data_ptr = (uint8_t *)vec->data + last_item_offset;
 
+    dbg("start_data_ptr %p", (void *)start_data_ptr);
     for (size_t i = 0; i < reqd_capacity; i++) {
+      uint8_t *data_ptr = start_data_ptr + i;
       uint8_t val = *((uint8_t *)els + i);
-      *(data_ptr + i) = val;
+
+      *data_ptr = val;
+
+      dbg("data_ptr %p, val %d", (void *)data_ptr, *data_ptr);
     }
 
     vec->length += reqd_capacity / vec->element_sz;
-    /* if (vec->) */
   } else {
     dbg("vec_push:input>Invalid vector");
   }
+  return vec;
 }
 
 #endif // VEC_IMPLEMENTATION
--- libs/vec_test.c
+++ libs/vec_test.c
@@ -2,18 +2,22 @@
 
 #include "indirect_libs_include.h"
 
-/* #define test(...) broski((__VA_ARGS__), (__VA_ARGS__) == NULL ? 0 : "omg") */
+/* #define bruh(type, ...) type potato[] = {__VA_ARGS__} */
 
-// gcc -DZDX_TRACE_ENABLE -g -Wall -pedantic -Wextra -Wdeprecated vec_test.c -o ./vec_test && leaks --atExit -- ./vec_test
+// DEBUG
+// gcc -DZDX_TRACE_ENABLE -g -Wall -pedantic -Wextra -Wdeprecated -Wno-gnu-statement-expression-from-macro-expansion vec_test.c -o ./vec_test && leaks --atExit -- ./vec_test
+// RELEASE
+// gcc -O3 -Wall -Wno-gnu-statement-expression-from-macro-expansion ./vec_test.c -o ./vec_test
 int main(void)
 {
-  /* test(NULL); */
-  /* test({1, 2, 3}); */
-  /* test((int []){1, 2, 3}); */
-
+  /* bruh(int, 1, 2, 3); */
+  /* printf("%d %d %d", potato[0], potato[1], potato[2]); */
 
   Vec v1 = vec_create(NULL);
-  /* Vec v2 = vec_create((char []){0}); */
+  vec_free(v1);
+
+  /* Vec v2 = vec_create((char []){}); */
+  /* vec_free(v2); */
 
   // this does 3 stack allocation for {1, 2, 3} due to how
   // vec_create macro is written unfortunately
@@ -22,19 +26,15 @@ int main(void)
 
   if (v3.valid) {
     printf("11111111\ndata[0] %d\ndata[1] %d\ndata[2] %d\ndata[3] %d\nvec length %zu\n", a[0], a[1], a[2], a[3], v3.length);
-    vec_push(&v3, 200);
+    a = vec_push(v3, 200);
     printf("22222222\ndata[0] %d\ndata[1] %d\ndata[2] %d\ndata[3] %d\nvec length %zu\n", a[0], a[1], a[2], a[3], v3.length);
-    vec_push(&v3, 100, 900);
-    printf("33333333\ndata[0] %d\ndata[1] %d\ndata[2] %d\ndata[3] %d\ndata[4] %d\ndata[5] %d\nvec length %zu\n",
-           a[0], a[1], a[2], a[3], a[4], a[5], v3.length);
+    a = vec_push(v3, 100, 50, 60);
+    printf("33333333\ndata[0] %d\ndata[1] %d\ndata[2] %d\ndata[3] %d\ndata[4] %d\ndata[5] %d\ndata[6] %d\nvec length %zu\n",
+           a[0], a[1], a[2], a[3], a[4], a[5], a[6], v3.length);
 
-
-    /* vec_ */
 
   }
 
-  vec_free(v1);
-  /* vec_free(v2); */
   vec_free(v3);
 
   return 0;
